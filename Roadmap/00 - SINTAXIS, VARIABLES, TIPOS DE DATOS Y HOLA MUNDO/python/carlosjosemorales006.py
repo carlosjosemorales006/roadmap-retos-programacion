f23a3800b63296b1c80e105c8cf4bddab913921d
Carlos Morales <carlosjosemoralesdejesus@gmail.com>
2024-08-24 20:47:09 -0400

    Reto 00
    MI solucion al reto #00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/carlosjosemorales006.py
@@ -0,0 +1,24 @@
+
+
+# python.org
+
+# comentario de una sola linea
+
+''' Comentario
+de
+varias lineas
+'''
+
+
+
+variable = 'Hola Mundo'
+CONSTANTE = 3.14
+
+print(f'{variable}, el PI es igual a: {CONSTANTE}')
+
+cadenas = 'Soy una cadena de texto'
+entero = 25
+decimal = 2.5
+booleanos = True
+
+print('!Hola, PYTHON!')
